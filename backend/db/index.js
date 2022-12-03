623e1b394e8cca0be83fc090f9a090bf2b2e9500
rakesh-fullstack <rakesh.kota@fullstack369.com>
2022-12-03 23:13:20 +1100

    Changed the Sequelize constructor

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,22 +1,31 @@
+const { pg } = require("pg");
 require("dotenv").config();
-
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.PGDATABASE,
-  process.env.PGUSER,
-  process.env.PGPASSWORD,
-  {
-    host: process.env.PGHOST,
-    dialect: "postgres",
-  }
-);
+const HOST = process.env.PGHOST;
+const DB = process.env.PGDATABASE;
+const USER = process.env.PGUSER;
+const PWD = process.env.PGPASSWORD;
+
+const sequelize = new Sequelize(`postgres://${USER}:${PWD}@${HOST}:5432/${DB}`, {
+  dialectModule: pg,
+});
+
+// const sequelize = new Sequelize(
+//   process.env.PGDATABASE,
+//   process.env.PGUSER,
+//   process.env.PGPASSWORD,
+//   {
+//     host: process.env.PGHOST,
+//     dialect: "postgres",
+//   }
+// );
 
 try {
   sequelize.authenticate();
   console.log("file: index.js ~ Succesfully connected to SQL");
 } catch (err) {
-  console.log("file: index.js ~ line 21 ~ Error while connecting to sql", err);
+  console.log("file: index.js ~ line 28 ~ Error while connecting to sql", err);
 }
 
 module.exports = sequelize;
